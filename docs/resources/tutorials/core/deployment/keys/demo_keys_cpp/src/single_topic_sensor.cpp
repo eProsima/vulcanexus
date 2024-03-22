468a15a48606e990b85a257d3878da394eb4d0f6
Mario Dominguez <mariodominguez@eprosima.com>
2024-03-22 12:25:47 +0100

    Refs #20512: Apply licenses to source code
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>

--- docs/resources/tutorials/core/deployment/keys/demo_keys_cpp/src/single_topic_sensor.cpp
+++ docs/resources/tutorials/core/deployment/keys/demo_keys_cpp/src/single_topic_sensor.cpp
@@ -1,3 +1,4 @@
+// Copyright 2024 Proyectos y Sistemas de Mantenimiento SL (eProsima).
 // Copyright 2024 Open Source Robotics Foundation, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
