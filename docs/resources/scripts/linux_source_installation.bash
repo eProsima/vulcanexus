3c619a130e4f64b27e224aa0e5adba951458c0f9
Raul Sanchez-Mateos Lizano <raul@eprosima.com>
2024-04-12 02:09:43 -0700

    Remove duplicated packages in installation from sources script (#202)
    Signed-off-by: Raul Sanchez-Mateos <raul@eprosima.com>

--- docs/resources/scripts/linux_source_installation.bash
+++ docs/resources/scripts/linux_source_installation.bash
@@ -95,7 +95,22 @@ rm -rf \
     src/eProsima/foonathan_memory_vendor/ \
     src/ros2/rosidl_typesupport_fastrtps/ \
     src/ros2/rosidl_dynamic_typesupport_fastrtps \
-    src/ros2/rmw_fastrtps/
+    src/ros2/rmw_fastrtps/ \
+    src/ros2/rosidl/rosidl_adapter/ \
+    src/ros2/rosidl/rosidl_cli/ \
+    src/ros2/rosidl/rosidl_cmake/ \
+    src/ros2/rosidl/rosidl_generator_c/ \
+    src/ros2/rosidl/rosidl_generator_cpp/ \
+    src/ros2/rosidl/rosidl_generator_tests/ \
+    src/ros2/rosidl/rosidl_generator_type_description/ \
+    src/ros2/rosidl/rosidl_parser/ \
+    src/ros2/rosidl/rosidl_pycommon/ \
+    src/ros2/rosidl/rosidl_runtime_c/ \
+    src/ros2/rosidl/rosidl_runtime_cpp/ \
+    src/ros2/rosidl/rosidl_typesupport_interface/ \
+    src/ros2/rosidl/rosidl_typesupport_introspection_c/ \
+    src/ros2/rosidl/rosidl_typesupport_introspection_cpp/ \
+    src/ros2/rosidl/rosidl_typesupport_introspection_tests/
 
 # Get Vulcanexus sources
 wget https://raw.githubusercontent.com/eProsima/vulcanexus/iron/vulcanexus.repos
