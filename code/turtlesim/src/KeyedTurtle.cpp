bfdf86c4ba6a44a07bc673dfbe0f9577ee4250a9
Irene Bandera <irenebandera@eprosima.com>
2024-05-27 09:17:18 +0200

    Add code to docs_turtlesim package
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>

---
+++ code/turtlesim/src/KeyedTurtle.cpp
@@ -0,0 +1,199 @@
+/*
+ * Copyright (c) 2009, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+// Copyright 2024 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file KeyedTurtle.cpp
+ */
+
+#include "docs_turtlesim/KeyedTurtle.hpp"
+
+#include <math.h>
+
+#include <QColor>
+
+#define DEFAULT_PEN_R 0xb3
+#define DEFAULT_PEN_G 0xb8
+#define DEFAULT_PEN_B 0xff
+
+#define PI 3.14159265
+#define TWO_PI 2.0 * PI
+
+
+static double normalizeAngle(double angle)
+{
+  return angle - (TWO_PI * std::floor((angle + PI) / (TWO_PI)));
+}
+
+Turtle::Turtle(const long id, const QImage& turtle_image, const QPointF& pos, float orient)
+: Node("keyed_turtle" + std::to_string(id))
+, key_(id)
+, turtle_image_(turtle_image)
+, pos_(pos)
+, orient_(orient)
+, lin_vel_x_(0.0)
+, lin_vel_y_(0.0)
+, ang_vel_(0.0)
+, pen_on_(true)
+, pen_(QColor(DEFAULT_PEN_R, DEFAULT_PEN_G, DEFAULT_PEN_B))
+{
+  pen_.setWidth(3);
+
+  // Initialize a subscription with a content filter to receive data with the key of the turtle
+  rclcpp::SubscriptionOptions sub_options;
+  sub_options.content_filter_options.filter_expression = "key = %0";
+  sub_options.content_filter_options.expression_parameters = {
+    id
+  };
+
+  // Create a subscription for the velocity of the turtle
+  velocity_sub_ = create_subscription<docs_turtlesim::msg::KeyedTwist>(
+    "/cmd_vel",
+    rclcpp::QoS(rclcpp::KeepLast(1)).reliable().transient_local(),
+    std::bind(&Turtle::velocityCallback, this, std::placeholders::_1),
+    sub_options);
+
+  // Create a publisher for the pose of the turtle
+  pose_pub_ = create_publisher<docs_turtlesim::msg::KeyedPose>(
+    "/pose",
+    rclcpp::QoS(rclcpp::KeepLast(1)).reliable().transient_local());
+
+  last_command_time_ = now();
+
+  meter_ = turtle_image_.height();
+  rotateImage();
+
+  RCLCPP_INFO(get_logger(), "Starting turtlesim with node name %s", get_fully_qualified_name());
+}
+
+Turtle::~Turtle()
+{
+  RCLCPP_INFO(get_logger(), "Shutting down turtlesim with node name %s", get_fully_qualified_name());
+}
+
+void Turtle::velocityCallback(const docs_turtlesim::msg::KeyedTwist::ConstSharedPtr vel)
+{
+  last_command_time_ = now();
+  lin_vel_x_ = vel->linear.x;
+  lin_vel_y_ = vel->linear.y;
+  ang_vel_ = vel->angular.z;
+}
+
+void Turtle::rotateImage()
+{
+  QTransform transform;
+  transform.rotate(-orient_ * 180.0 / PI + 90.0);
+  turtle_rotated_image_ = turtle_image_.transformed(transform);
+}
+
+bool Turtle::update(double dt, QPainter& path_painter, const QImage& path_image, qreal canvas_width, qreal canvas_height)
+{
+  bool modified = false;
+  qreal old_orient = orient_;
+
+  if (now() - last_command_time_ > rclcpp::Duration(1.0, 0))
+  {
+    lin_vel_x_ = 0.0;
+    lin_vel_y_ = 0.0;
+    ang_vel_ = 0.0;
+  }
+
+  QPointF old_pos = pos_;
+
+  orient_ = orient_ + ang_vel_ * dt;
+  // Keep orient_ between -pi and +pi
+  orient_ = normalizeAngle(orient_);
+  pos_.rx() += std::cos(orient_) * lin_vel_x_ * dt
+             - std::sin(orient_) * lin_vel_y_ * dt;
+  pos_.ry() -= std::cos(orient_) * lin_vel_y_ * dt
+             + std::sin(orient_) * lin_vel_x_ * dt;
+
+  // Clamp to screen size
+  if (pos_.x() < 0 || pos_.x() > canvas_width ||
+      pos_.y() < 0 || pos_.y() > canvas_height)
+  {
+    RCLCPP_WARN(get_logger(), "Oh no! I hit the wall! (Clamping from [x=%f, y=%f])", pos_.x(), pos_.y());
+  }
+
+  pos_.setX(std::min(std::max(static_cast<double>(pos_.x()), 0.0), static_cast<double>(canvas_width)));
+  pos_.setY(std::min(std::max(static_cast<double>(pos_.y()), 0.0), static_cast<double>(canvas_height)));
+
+  if (orient_ != old_orient)
+  {
+    rotateImage();
+    modified = true;
+  }
+  if (pos_ != old_pos)
+  {
+    if (pen_on_)
+    {
+      path_painter.setPen(pen_);
+      path_painter.drawLine(pos_ * meter_, old_pos * meter_);
+    }
+    modified = true;
+  }
+
+  if (modified)
+  {
+    // Publish pose of the turtle
+    auto p = std::make_unique<docs_turtlesim::msg::KeyedPose>();
+    p->key = key_;
+    p->x = pos_.x();
+    p->y = canvas_height - pos_.y();
+    p->theta = orient_;
+    p->linear_velocity = std::sqrt(lin_vel_x_ * lin_vel_x_ + lin_vel_y_ * lin_vel_y_);
+    p->angular_velocity = ang_vel_;
+    pose_pub_->publish(std::move(p));
+
+    RCLCPP_INFO(get_logger(), "Turtle [%ld]: pos_x: %f pos_y: %f theta: %f", key_, pos_.x(), pos_.y(), orient_);
+  }
+
+  return modified;
+}
+
+void Turtle::paint(QPainter& painter)
+{
+  QPointF p = pos_ * meter_;
+  p.rx() -= 0.5 * turtle_rotated_image_.width();
+  p.ry() -= 0.5 * turtle_rotated_image_.height();
+  painter.drawImage(p, turtle_rotated_image_);
+}
---
+++ code/turtlesim/src/controller_keyed_turtle.cpp
@@ -0,0 +1,231 @@
+// Copyright 2024 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file controller_keyed_turtle.cpp
+ */
+
+#include <stdexcept>
+#include <termios.h>
+
+#include <rclcpp/rclcpp.hpp>
+#include <docs_turtlesim/msg/keyed_twist.hpp>
+#include <docs_turtlesim/msg/keyed_pose.hpp>
+
+static constexpr char KEYCODE_RIGHT = 0x43;
+static constexpr char KEYCODE_LEFT = 0x44;
+static constexpr char KEYCODE_UP = 0x41;
+static constexpr char KEYCODE_DOWN = 0x42;
+static constexpr char KEYCODE_q = 0x71;
+static constexpr char KEYCODE_Q = 0x51;
+static constexpr char KEYCODE_1 = 0x31;
+static constexpr char KEYCODE_2 = 0x32;
+static constexpr char KEYCODE_3 = 0x33;
+
+#define SCALE_ANGULAR 2.0
+#define SCALE_LINEAR 2.0
+
+
+class KeyboardReader
+{
+public:
+  KeyboardReader()
+  {
+    // get the console in raw mode
+    if (tcgetattr(0, &cooked_) < 0)
+    {
+      throw std::runtime_error("Failed to get old console mode");
+    }
+    struct termios raw;
+    memcpy(&raw, &cooked_, sizeof(struct termios));
+    raw.c_lflag &=~ (ICANON | ECHO);
+    // Setting a new line, then end of file
+    raw.c_cc[VEOL] = 1;
+    raw.c_cc[VEOF] = 2;
+    raw.c_cc[VTIME] = 1;
+    raw.c_cc[VMIN] = 0;
+    if (tcsetattr(0, TCSANOW, &raw) < 0)
+    {
+      throw std::runtime_error("Failed to set new console mode");
+    }
+  }
+
+  char readOne()
+  {
+    char c = 0;
+
+    int rc = read(0, &c, 1);
+    if (rc < 0)
+    {
+      throw std::runtime_error("read failed");
+    }
+
+    return c;
+  }
+
+  ~KeyboardReader()
+  {
+    tcsetattr(0, TCSANOW, &cooked_);
+  }
+
+private:
+  struct termios cooked_;
+};
+
+
+class ControlKeyedTurtle : public rclcpp::Node
+{
+public:
+  ControlKeyedTurtle()
+  : Node("controller_keyed_turtle")
+  , key_(0)
+  {
+    // Create a publisher for the velocity
+    velocity_pub_ = create_publisher<docs_turtlesim::msg::KeyedTwist>(
+      "/cmd_vel",
+      rclcpp::QoS(rclcpp::KeepLast(1)).reliable().transient_local());
+
+    // Initialize a subscription with a content filter to receive data from turtle 1 to 2
+    rclcpp::SubscriptionOptions sub_options;
+    sub_options.content_filter_options.filter_expression = "key <= 2";
+
+    // Create a subscriber for the pose
+    pose_sub_ = create_subscription<docs_turtlesim::msg::KeyedPose>(
+      "/pose",
+      rclcpp::QoS(rclcpp::KeepLast(1)).reliable().transient_local(),
+      std::bind(&ControlKeyedTurtle::callbackPose, this, std::placeholders::_1),
+      sub_options);
+
+    puts("Reading from keyboard");
+    puts("---------------------------");
+    puts("Select turtle to control with 1, 2 or 3.");
+    puts("Use arrow keys to move the turtle.");
+    puts("'Q' to quit.");
+  }
+
+  int readKey()
+  {
+    char c;
+
+    // get the next event from the keyboard
+    try
+    {
+      c = input_.readOne();
+    }
+    catch (const std::runtime_error & e)
+    {
+      return -1;
+    }
+
+    double linear = 0.0;
+    double angular = 0.0;
+
+    RCLCPP_DEBUG(get_logger(), "value: 0x%02X\n", c);
+
+    switch(c)
+    {
+    case KEYCODE_LEFT:
+      RCLCPP_INFO(get_logger(), "LEFT");
+      angular = 1.0;
+      break;
+    case KEYCODE_RIGHT:
+      RCLCPP_INFO(get_logger(), "RIGHT");
+      angular = -1.0;
+      break;
+    case KEYCODE_UP:
+      RCLCPP_INFO(get_logger(), "UP");
+      linear = 1.0;
+      break;
+    case KEYCODE_DOWN:
+      RCLCPP_INFO(get_logger(), "DOWN");
+      linear = -1.0;
+      break;
+    case KEYCODE_q:
+    case KEYCODE_Q:
+      RCLCPP_INFO(get_logger(), "Quit");
+      return 0;
+    case KEYCODE_1:
+      RCLCPP_INFO(get_logger(), "Selecting turtle 1");
+      key_ = 1;
+      break;
+    case KEYCODE_2:
+      RCLCPP_INFO(get_logger(), "Selecting turtle 2");
+      key_ = 2;
+      break;
+    case KEYCODE_3:
+      RCLCPP_INFO(get_logger(), "Selecting turtle 3");
+      key_ = 3;
+      break;
+    default:
+      // This can happen if the read returned when there was no data, or
+      // another key was pressed.  In these cases, just silently ignore the
+      // press.
+      break;
+    }
+
+    if (linear != 0.0 || angular != 0.0)
+    {
+      if (key_ == 0)
+      {
+        RCLCPP_INFO(get_logger(), "No turtle selected.");
+      }
+      else
+      {
+        docs_turtlesim::msg::KeyedTwist twist;
+        twist.key = key_;
+        twist.angular.z = SCALE_ANGULAR * angular;
+        twist.linear.x = SCALE_LINEAR * linear;
+        velocity_pub_->publish(twist);
+      }
+    }
+
+    return 1;
+  }
+
+private:
+
+  void callbackPose(const docs_turtlesim::msg::KeyedPose::SharedPtr pose) const
+  {
+    RCLCPP_INFO(get_logger(), "Turtle [%d] is at x: %f, y: %f, theta: %f",
+      pose->key, pose->x, pose->y, pose->theta);
+  }
+
+  rclcpp::Publisher<docs_turtlesim::msg::KeyedTwist>::SharedPtr velocity_pub_;
+  rclcpp::Subscription<docs_turtlesim::msg::KeyedPose>::SharedPtr pose_sub_;
+
+  long key_;
+  KeyboardReader input_;
+};
+
+
+int main(int argc, char** argv)
+{
+  rclcpp::init(argc, argv);
+
+  auto control_node = std::make_shared<ControlKeyedTurtle>();
+
+  while (rclcpp::ok()) {
+
+    int rc = control_node->readKey();
+    if (rc != 1) {
+      break;
+    }
+
+    rclcpp::spin_some(control_node);
+  }
+
+  rclcpp::shutdown();
+
+  return 0;
+}
